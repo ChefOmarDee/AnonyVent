a9ae9c222172ddf0fca415610c2e3a8ca4c5d83d
Carol0427 <carolraul27@gmail.com>
2024-06-23 11:13:58 -0400

    *

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,39 @@
-import tapeImg from "../images/retro-tape-icon.png";
-import micImg from "../images/mic-1-removebg-preview (1).png";
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
-import React from "react";
+import micImg from "../images/mic-1-removebg-preview (1).png";
+import tapeImg from "../images/retro-tape-icon.png";
 import "./Home.css";
+import { UserContext } from "./UserContext";
 
 const Home = () => {
+	const [docs, setDocs] = useState([]);
+	const [titles, setTitles] = useState([]);
+	const { user, setUser } = useUserContext();
+
+	useEffect(() => {
+        const fetchDocs = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/get'); // Update the URL with your server URL
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+				const titles = data.map(doc => doc.title); // Extract titles from each document
+				setTitles(titles);
+                setDocs(data);
+                console.log(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchDocs();
+    }, []); // Empty dependency array ensures this effect runs only once
+
 	return (
 		<>
 			<main>
 				<div id="tapes-container">
-					<div id="tapes" style={{ position: "relative" }}>
+					{/* <div id="tapes" style={{ position: "relative" }}>
 						<div className="tape">
 							<img
 								className="tapeDisappear"
@@ -18,7 +42,7 @@ const Home = () => {
 								width="250"
 								height="250"
 							/>
-							<div className="tape-text">afasdf</div>
+							<div className="tape-text"></div>
 						</div>
 						<div className="tape">
 							<img
@@ -39,7 +63,21 @@ const Home = () => {
 								height="250"
 							/>
 							<div className="tape-text">afasdf</div>
-						</div>
+						</div> */}
+						<div id="tapes" style={{ position: "relative" }}>
+						{docs.map((doc, index) => (
+                            <Link to={"viewvent/" + doc._id}><div className="tape" key={index}>
+                                <img
+                                    className="tapeDisappear"
+                                    src={tapeImg}
+                                    alt={`tape${index}`}
+                                    width="250"
+                                    height="250"
+                                />
+                                <div className="tape-text">{doc.title}</div>
+                            </div>
+							</Link>
+                        ))}
 					</div>
 					<Link to="/recordvent">
 						<div id="record-button">
@@ -47,6 +85,7 @@ const Home = () => {
 						</div>
 					</Link>
 				</div>
+				{/* <button onClick={fetchTitles}>Fetch Titles</button> */}
 			</main>
 		</>
 	);
--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const AWS = require("aws-sdk");
 const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
+const { MongoClient } = require('mongodb');
+// MongoDB connection string
+const uri = process.env.CONNECTION_STR; 
+const client = new MongoClient(uri);
 
 const app = express();
 const port = 8080;
@@ -13,89 +17,126 @@ const port = 8080;
 app.use(cors());
 
 // Set up multer storage
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, "uploads/");
-	},
-	filename: function (req, file, cb) {
-		cb(
-			null,
-			file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-		);
-	},
-});
+// const storage = multer.diskStorage({
+// 	destination: function (req, file, cb) {
+// 		cb(null, "uploads/");
+// 	},
+// 	filename: function (req, file, cb) {
+// 		cb(
+// 			null,
+// 			file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+// 		);
+// 	},
+// });
 
-const upload = multer({
-	storage: storage,
-	fileFilter: function (req, file, cb) {
-		const allowedTypes = ["audio/mpeg", "audio/mp3", "audio/mpg"];
-		if (!allowedTypes.includes(file.mimetype)) {
-			return cb("Error: Only .mp3 files are allowed!");
-		}
+// const upload = multer({
+// 	storage: storage,
+// 	fileFilter: function (req, file, cb) {
+// 		const allowedTypes = ["audio/mpeg", "audio/mp3", "audio/mpg"];
+// 		if (!allowedTypes.includes(file.mimetype)) {
+// 			return cb("Error: Only .mp3 files are allowed!");
+// 		}
 
-		const extname = path.extname(file.originalname).toLowerCase();
-		if (extname !== ".mp3") {
-			return cb("Error: Only .mp3 files are allowed!");
-		}
+// 		const extname = path.extname(file.originalname).toLowerCase();
+// 		if (extname !== ".mp3") {
+// 			return cb("Error: Only .mp3 files are allowed!");
+// 		}
 
-		cb(null, true);
-	},
-});
+// 		cb(null, true);
+// 	},
+// });
 
-// Create the uploads directory if it doesn't exist
-const dir = "./uploads";
-if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir);
+// // Create the uploads directory if it doesn't exist
+// const dir = "./uploads";
+// if (!fs.existsSync(dir)) {
+// 	fs.mkdirSync(dir);
+// }
+
+// // Configure AWS SDK
+// AWS.config.update({
+// 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+// 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// 	region: process.env.AWS_REGION,
+// });
+
+// const s3 = new AWS.S3();
+
+// // Route to handle file upload and upload to S3
+// app.post("/upload", upload.single("mp3file"), (req, res) => {
+// 	if (!req.file) {
+// 		return res.status(400).send("No file uploaded.");
+// 	}
+
+// 	const recordingTime = req.body.recordingTime; // Extract recording time from request body
+// 	console.log(`Recording Time: ${recordingTime} seconds`);
+
+// 	const fileContent = fs.readFileSync(req.file.path);
+// 	// const params = {
+// 	//     Bucket: process.env.S3_BUCKET_NAME,
+// 	//     Key: `uploads/${req.file.filename}`, // File name you want to save as in S3
+// 	//     Body: fileContent,
+// 	//     ContentType: req.file.mimetype,
+// 	//     ACL: 'public-read' // File permissions
+// 	// };
+
+// 	// s3.upload(params, (err, data) => {
+// 	//     // Delete the local file after upload
+// 	//     fs.unlink(req.file.path, (err) => {
+// 	//         if (err) {
+// 	//             console.error('Error deleting the file:', err);
+// 	//             return res.status(500).send('Failed to delete local file.');
+// 	//         }
+// 	//     });
+
+// 	//     if (err) {
+// 	//         console.error('Error uploading file:', err);
+// 	//         return res.status(500).send('Failed to upload file.');
+// 	//     }
+
+// 	res.send(
+// 		`File uploaded successfully. Recording time: ${recordingTime} seconds`
+// 	);
+// 	// });
+// });
+
+async function main() {
+  try {
+    // Connect to MongoDB cluster
+    await client.connect();
+    const database = client.db('AnonyVent');
+    // Access the collection
+    const collection = database.collection('Vents');
+    const randomDocuments = await collection.aggregate([{ $sample: { size: 3 } }]).toArray();
+    //const titles = randomDocuments.map(doc => doc.title);
+	//console.log(titles);
+	console.log(randomDocuments);
+    return randomDocuments;
+  } catch (e) {
+    console.log(e);
+    throw e; // Re-throw the error to be caught by the caller
+  } finally {
+    await client.close();
+  }
 }
-
-// Configure AWS SDK
-AWS.config.update({
-	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-	region: process.env.AWS_REGION,
+main().then(() => {
+	console.log('main function completed');
+  }).catch(error => {
+	console.error('Error in main function:', error);
+  });
+app.get('/get', async (req, res) => {
+  try {
+    const docs = await main();
+	//const titles = ["hello", "yappa", "hey"];
+	//console.log(titles);
+	//const hello = "hello";
+    res.json(docs); // Send the titles as JSON response
+  } catch (e) {
+    console.error('Error fetching titles from MongoDB:', e);
+    res.status(500).json({ error: 'Internal Server Error' }); // Send an error response
+  }
 });
-
-const s3 = new AWS.S3();
-
-// Route to handle file upload and upload to S3
-app.post("/upload", upload.single("mp3file"), (req, res) => {
-	if (!req.file) {
-		return res.status(400).send("No file uploaded.");
-	}
-
-	const recordingTime = req.body.recordingTime; // Extract recording time from request body
-	console.log(`Recording Time: ${recordingTime} seconds`);
-
-	const fileContent = fs.readFileSync(req.file.path);
-	// const params = {
-	//     Bucket: process.env.S3_BUCKET_NAME,
-	//     Key: `uploads/${req.file.filename}`, // File name you want to save as in S3
-	//     Body: fileContent,
-	//     ContentType: req.file.mimetype,
-	//     ACL: 'public-read' // File permissions
-	// };
-
-	// s3.upload(params, (err, data) => {
-	//     // Delete the local file after upload
-	//     fs.unlink(req.file.path, (err) => {
-	//         if (err) {
-	//             console.error('Error deleting the file:', err);
-	//             return res.status(500).send('Failed to delete local file.');
-	//         }
-	//     });
-
-	//     if (err) {
-	//         console.error('Error uploading file:', err);
-	//         return res.status(500).send('Failed to upload file.');
-	//     }
-
-	res.send(
-		`File uploaded successfully. Recording time: ${recordingTime} seconds`
-	);
-	// });
-});
-
 // Start the server
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}/`);
 });
+
