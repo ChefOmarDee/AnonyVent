b90c9e3fd8cb08e26696b0d23292fd1e4ea1c110
ChefOmarDee <ChefOmarDee@gmail.com>
2024-06-23 11:13:57 -0400

    yo

--- client/src/pages/RecordVent.jsx
+++ client/src/pages/RecordVent.jsx
@@ -1,130 +1,185 @@
-import React, { useState, useRef, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useRef, useEffect } from "react";
+import axios from "axios";
 
 const RecordVent = () => {
-    const [isRecording, setIsRecording] = useState(false);
-    const [isPaused, setIsPaused] = useState(false);
-    const [audioURL, setAudioURL] = useState('');
-    const [audioBlob, setAudioBlob] = useState(null);
-    const [recordingTime, setRecordingTime] = useState(0);
-    const [isUploading, setIsUploading] = useState(false);
-    const mediaRecorderRef = useRef(null);
-    const audioChunksRef = useRef([]);
-    const timerRef = useRef(null);
-    const finalRecordingTimeRef = useRef(0);
+	const [isRecording, setIsRecording] = useState(false);
+	const [isPaused, setIsPaused] = useState(false);
+	const [audioURL, setAudioURL] = useState("");
+	const [audioBlob, setAudioBlob] = useState(null);
+	const [recordingTime, setRecordingTime] = useState(0);
+	const [isUploading, setIsUploading] = useState(false);
+	const [uploadingResponse, setUploadingResponse] = useState("");
+	const [processing, setProcessing] = useState(false); // State for processing state
+	const [title, setTitle] = useState(""); // State for recording title
+	const [showResponse, setShowResponse] = useState(false); // State to show response message
+	const mediaRecorderRef = useRef(null);
+	const audioChunksRef = useRef([]);
+	const timerRef = useRef(null);
+	const finalRecordingTimeRef = useRef(0);
 
-    useEffect(() => {
-        if (isRecording && recordingTime >= 180) {
-            stopRecording();
-        }
-    }, [recordingTime]);
+	useEffect(() => {
+		// Clean up function for stopping audio when component unmounts
+		return () => {
+			if (audioURL) {
+				URL.revokeObjectURL(audioURL); // Revoke the Object URL to stop audio
+			}
+		};
+	}, [audioURL]);
 
-    const startRecording = async () => {
-        try {
-            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-                mediaRecorderRef.current = new MediaRecorder(stream);
+	useEffect(() => {
+		if (isRecording && recordingTime >= 180) {
+			stopRecording();
+		}
+	}, [recordingTime]);
 
-                mediaRecorderRef.current.ondataavailable = event => {
-                    if (event.data.size > 0) {
-                        audioChunksRef.current.push(event.data);
-                    }
-                };
+	const startRecording = async () => {
+		try {
+			if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+				const stream = await navigator.mediaDevices.getUserMedia({
+					audio: true,
+				});
+				mediaRecorderRef.current = new MediaRecorder(stream);
 
-                mediaRecorderRef.current.onstop = () => {
-                    const audioBlob = new Blob(audioChunksRef.current, { type: 'audio/mp3' });
-                    const audioUrl = URL.createObjectURL(audioBlob);
-                    setAudioURL(audioUrl);
-                    setAudioBlob(audioBlob);
-                    audioChunksRef.current = [];
-                    clearInterval(timerRef.current);
-                };
+				mediaRecorderRef.current.ondataavailable = (event) => {
+					if (event.data.size > 0) {
+						audioChunksRef.current.push(event.data);
+					}
+				};
 
-                mediaRecorderRef.current.start();
-                setIsRecording(true);
-                setIsPaused(false);
-                startTimer();
-            }
-        } catch (error) {
-            console.error('Error accessing microphone:', error);
-        }
-    };
+				mediaRecorderRef.current.onstop = () => {
+					const audioBlob = new Blob(audioChunksRef.current, {
+						type: "audio/mp3",
+					});
+					const audioUrl = URL.createObjectURL(audioBlob);
+					setAudioURL(audioUrl);
+					setAudioBlob(audioBlob);
+					audioChunksRef.current = [];
+					clearInterval(timerRef.current);
+				};
 
-    const startTimer = () => {
-        timerRef.current = setInterval(() => {
-            setRecordingTime(prevTime => prevTime + 1);
-        }, 1000);
-    };
+				mediaRecorderRef.current.start();
+				setIsRecording(true);
+				setIsPaused(false);
+				setRecordingTime(0); // Reset recording time
+				startTimer();
+			}
+		} catch (error) {
+			console.error("Error accessing microphone:", error);
+		}
+	};
 
-    const pauseRecording = () => {
-        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
-            mediaRecorderRef.current.pause();
-            setIsPaused(true);
-            clearInterval(timerRef.current);
-        }
-    };
+	const startTimer = () => {
+		timerRef.current = setInterval(() => {
+			setRecordingTime((prevTime) => prevTime + 1);
+		}, 1000);
+	};
 
-    const resumeRecording = () => {
-        if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'paused') {
-            mediaRecorderRef.current.resume();
-            setIsPaused(false);
-            startTimer();
-        }
-    };
+	const pauseRecording = () => {
+		if (
+			mediaRecorderRef.current &&
+			mediaRecorderRef.current.state === "recording"
+		) {
+			mediaRecorderRef.current.pause();
+			setIsPaused(true);
+			clearInterval(timerRef.current);
+		}
+	};
 
-    const stopRecording = () => {
-        if (mediaRecorderRef.current) {
-            mediaRecorderRef.current.stop();
-            setIsRecording(false);
-            setIsPaused(false);
-            clearInterval(timerRef.current);
-            finalRecordingTimeRef.current = recordingTime; // Store the final recording time
-            console.log('Final recording time:', finalRecordingTimeRef.current); // Debugging
-        }
-    };
+	const resumeRecording = () => {
+		if (
+			mediaRecorderRef.current &&
+			mediaRecorderRef.current.state === "paused"
+		) {
+			mediaRecorderRef.current.resume();
+			setIsPaused(false);
+			startTimer();
+		}
+	};
 
-    const uploadAudio = async () => {
-        if (audioBlob) {
-            setIsUploading(true);
-            const formData = new FormData();
-            formData.append('mp3file', audioBlob, 'recording.mp3');
-            formData.append('recordingTime', finalRecordingTimeRef.current); // Use the stored final recording time
+	const stopRecording = () => {
+		if (mediaRecorderRef.current) {
+			mediaRecorderRef.current.stop();
+			setIsRecording(false);
+			setIsPaused(false);
+			clearInterval(timerRef.current);
+			finalRecordingTimeRef.current = recordingTime; // Store the final recording time
+		}
+	};
 
-            try {
-                const response = await axios.post('http://localhost:8080/upload', formData, {
-                    headers: {
-                        'Content-Type': 'multipart/form-data'
-                    }
-                });
-                console.log('File uploaded successfully:', response.data);
-                console.log('Uploaded recording time:', finalRecordingTimeRef.current); // Debugging
-                setAudioURL('');
-                setAudioBlob(null);
-                setIsUploading(false);
-            } catch (error) {
-                console.error('Error uploading file:', error);
-                setIsUploading(false);
-            }
-        }
-    };
+	const uploadAudio = async () => {
+		if (audioBlob && title.trim() !== "") {
+			// Check if title is not empty
+			setProcessing(true); // Start processing state
+			const formData = new FormData();
+			formData.append("mp3file", audioBlob, "recording.mp3");
+			formData.append("recordingTime", finalRecordingTimeRef.current);
+			formData.append("title", title); // Append title to FormData
 
-    return (
-        <div>
-            <h1>Audio Recorder</h1>
-            <button onClick={startRecording} disabled={isRecording}>Start Recording</button>
-            <button onClick={pauseRecording} disabled={!isRecording || isPaused}>Pause Recording</button>
-            <button onClick={resumeRecording} disabled={!isRecording || !isPaused}>Resume Recording</button>
-            <button onClick={stopRecording} disabled={!isRecording}>Stop Recording</button>
-            {audioURL && (
-                <div>
-                    <h2>Preview</h2>
-                    <audio controls src={audioURL}></audio>
-                    <button onClick={uploadAudio} disabled={isUploading}>Upload Recording</button>
-                </div>
-            )}
-            <p>Recording Time: {Math.floor(recordingTime / 60)}:{('0' + (recordingTime % 60)).slice(-2)} (Max: 3:00)</p>
-        </div>
-    );
+			try {
+				const response = await axios.post(
+					"http://localhost:8080/upload",
+					formData,
+					{
+						headers: {
+							"Content-Type": "multipart/form-data",
+						},
+					}
+				);
+
+				setUploadingResponse("File uploaded successfully");
+				console.log("File uploaded successfully:", response.data);
+				setAudioURL("");
+				setAudioBlob(null);
+				setProcessing(false); // End processing state
+				setShowResponse(true); // Show the response message
+			} catch (error) {
+				console.error("Error uploading file:", error);
+				setProcessing(false); // End processing state on error
+			}
+
+			setTimeout(() => {
+				setShowResponse(false); // Clear response after 7 seconds
+			}, 7000);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Audio Recorder</h1>
+			<button onClick={startRecording} disabled={isRecording}>
+				Start Recording
+			</button>
+			<button onClick={pauseRecording} disabled={!isRecording || isPaused}>
+				Pause Recording
+			</button>
+			<button onClick={resumeRecording} disabled={!isRecording || !isPaused}>
+				Resume Recording
+			</button>
+			<button onClick={stopRecording} disabled={!isRecording}>
+				Stop Recording
+			</button>
+			{audioURL && (
+				<div>
+					<h2>Preview</h2>
+					<audio controls src={audioURL}></audio>
+					<input
+						type="text"
+						placeholder="Enter recording title"
+						value={title}
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+					<button onClick={uploadAudio} disabled={isUploading || processing}>
+						{processing ? "Processing..." : "Upload Recording"}
+					</button>
+					{showResponse && <p>{uploadingResponse}</p>}
+				</div>
+			)}
+			<p>
+				Recording Time: {Math.floor(recordingTime / 60)}:
+				{("0" + (recordingTime % 60)).slice(-2)} (Max: 3:00)
+			</p>
+		</div>
+	);
 };
 
 export default RecordVent;
